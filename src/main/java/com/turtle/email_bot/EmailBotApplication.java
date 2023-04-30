e618f86bcdd2bad73af2898adb759669da6fa459
diuguide <everett.diuguid@gmail.com>
2023-04-30 19:17:53 -0400

    created spring project

---
+++ src/main/java/com/turtle/email_bot/EmailBotApplication.java
@@ -0,0 +1,13 @@
+package com.turtle.email_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/turtle/email_bot/EmailBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turtle.email_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
